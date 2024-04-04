626ced8246a0dd60e9560afbc7ce3d7916c9530f
daniel <dortega20@horizon.csueastbay.edu>
2024-04-04 11:16:36 -0700

    this is a change to the clicking fraagment so that an itemcan be clicked on and disappeard whil reaprating to a random spot

--- app/src/main/java/com/example/testing/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/testing/ui/home/HomeFragment.java
@@ -4,7 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import com.example.testing.R;
+import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -21,8 +22,21 @@ public class HomeFragment extends Fragment {
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
+        ImageView imageView = binding.getRoot().findViewById(R.id.imageView);
+
+        // Set OnClickListener to make the ImageView disappear when clicked
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setVisibility(View.GONE);
+            }
+        });
         return binding.getRoot();
+
     }
+
+
+
 
     @Override
     public void onDestroyView() {
