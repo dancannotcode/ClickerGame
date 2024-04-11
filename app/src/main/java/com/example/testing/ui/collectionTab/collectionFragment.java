b009bd027b2089f378fe2aa5ded4700686ae0db8
Nickolys <nsouza5@horizon.csueastbay.edu>
2024-04-11 09:08:03 -0700

    Got the Collections Page workingish -Collections page now shows list of question marks that can be replaced -Cant fully see them all but will be worked on

--- app/src/main/java/com/example/testing/ui/collectionTab/collectionFragment.java
+++ app/src/main/java/com/example/testing/ui/collectionTab/collectionFragment.java
@@ -6,12 +6,25 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.testing.R;
 import com.example.testing.databinding.FragmentCollectionBinding;
+import com.example.testing.virusModel;
+import com.example.testing.virus_RecyclerViewAdapter;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class collectionFragment extends Fragment{
 
+    ArrayList<virusModel> virusModel = new ArrayList<>();
+
+    int[] virusPicture = {R.drawable.question_mark,R.drawable.question_mark,R.drawable.question_mark,R.drawable.question_mark,R.drawable.question_mark,R.drawable.question_mark,R.drawable.question_mark};
     private FragmentCollectionBinding binding;
 
     @Override
@@ -22,9 +35,41 @@ public class collectionFragment extends Fragment{
         return binding.getRoot();
 
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        RecyclerView recyclerView = view.findViewById(R.id.virusRecyclerView);
+        setVirusModel();
+        virus_RecyclerViewAdapter adapter = new virus_RecyclerViewAdapter(getContext(),virusModel);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
+    }
+
+    private void setVirusModel(){
+        String[] virusName = getResources().getStringArray(R.array.virus_names);
+        String[] virusType = getResources().getStringArray(R.array.virus_type);
+        String[] virusColor = getResources().getStringArray(R.array.virus_color);
+
+        for (int i = 0; i < virusName.length; i++){
+            virusModel.add(new virusModel(virusColor[i],virusType[i],virusName[i],virusPicture[i]));
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/testing/virusModel.java
@@ -0,0 +1,33 @@
+package com.example.testing;
+
+public class virusModel {
+    String virusColor;
+    String virusType;
+    String virusName;
+    int image;
+
+
+    public virusModel(String virusColor, String virusType, String virusName, int image) {
+        this.virusColor = virusColor;
+        this.virusType = virusType;
+        this.virusName = virusName;
+        this.image = image;
+    }
+
+    public String getVirusColor() {
+        return virusColor;
+    }
+
+    public String getVirusType() {
+        return virusType;
+    }
+
+    public String getVirusName() {
+        return virusName;
+    }
+
+    public int getImage(){
+        return image;
+    }
+
+}
