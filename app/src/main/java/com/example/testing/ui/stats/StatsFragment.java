846bc51753add703875946cc555c7e74c2c5c5ec
Nickolys <nsouza5@horizon.csueastbay.edu>
2024-04-25 11:29:27 -0700

    Stat Page Suff

--- app/src/main/java/com/example/testing/ui/stats/StatsFragment.java
+++ app/src/main/java/com/example/testing/ui/stats/StatsFragment.java
@@ -1,4 +1,4 @@
-package com.example.testing.ui.dashboard;
+package com.example.testing.ui.stats;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.testing.databinding.FragmentDashboardBinding;
+import com.example.testing.databinding.FragmentStatsBinding;
 
-public class DashboardFragment extends Fragment {
+public class StatsFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentStatsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        StatsViewModel statsViewModel =
+                new ViewModelProvider(this).get(StatsViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentStatsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        statsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
